aa167870b83500a5955b7ef5ae5b8391a0c3c675
SheltonTolbert <60357311+SheltonTolbert@users.noreply.github.com>
2021-01-12 15:24:09 -0600

    implemented board state management and first person comtroller

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int[] boardState;
+    private int width = 8;
+    private int height = 8;
+
+    public Dictionary<string, int> pieceDictionary = new Dictionary<string, int>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPieceDictionary();
+        updateBoardState();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateBoardState() {
+        foreach (Transform child in transform)
+        {
+            string location = child.gameObject.name;
+            int piece = child.GetComponent<PieceCollider>().collidingPiece;
+            if (piece > 0)
+            {
+                Debug.Log(pieceNumToString(piece) + " to " + location);
+            }
+        }
+    }
+
+    // Match piece prefab names to an integer - makes working with the prefabs in the editor much easire. 
+    void initPieceDictionary() {
+        pieceDictionary.Add("BishopLight", 1);
+        pieceDictionary.Add("BishopDark", 2);
+        pieceDictionary.Add("KingDark", 3);
+        pieceDictionary.Add("KingLight", 4);
+        pieceDictionary.Add("KnightDark", 5);
+        pieceDictionary.Add("KnightLight", 6);
+        pieceDictionary.Add("PawnLight", 7);
+        pieceDictionary.Add("PawnDark", 8);
+        pieceDictionary.Add("QueenDark", 9);
+        pieceDictionary.Add("QueenLight", 10);
+        pieceDictionary.Add("RookDark", 11);
+        pieceDictionary.Add("RookLight", 12);
+    }
+
+    private string pieceNumToString(int pieceNum)
+    {
+        // Iterate through dictionary values to return the coorisponding Key Name
+        foreach (KeyValuePair<string, int> piece in pieceDictionary)
+        {
+            if (piece.Value == pieceNum)
+            {
+                return (piece.Key);
+            }
+        }
+        return ("Invalid Piece Number");
+    }
+}
---
+++ Assets/Scripts/PieceCollider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PieceCollider : MonoBehaviour
+{
+
+    
+    BoardManager boardManager;
+
+    public int collidingPiece = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject board = GameObject.Find("Board");
+        boardManager = board.GetComponent<BoardManager>();
+        //Debug.Log(boardManager.pieceDictionary["BishopLight"]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider piece)
+    {
+        //enter collision code here.
+        try
+        {
+            collidingPiece = boardManager.pieceDictionary[piece.name];
+            boardManager.updateBoardState();
+        }
+        catch(System.Exception e)
+        {
+            // Do nothing if the collider is not a game piece
+        }
+        }
+
+    private void OnTriggerExit(Collider other) {
+        collidingPiece = -1;
+    }
+
+    
+}
